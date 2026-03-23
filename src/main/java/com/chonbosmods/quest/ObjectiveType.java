e1d19f5c3885715b161439625035b43196eda664
chonbosmods <267707111+ChonbosMods@users.noreply.github.com>
2026-03-23 09:35:57 -0700

    refactor: narrow ObjectiveType to 4 MVP types (COLLECT_RESOURCES, KILL_MOBS, FETCH_ITEM, TALK_TO_NPC)

--- src/main/java/com/chonbosmods/quest/ObjectiveType.java
+++ src/main/java/com/chonbosmods/quest/ObjectiveType.java
@@ -1,11 +1,8 @@
 package com.chonbosmods.quest;
 
 public enum ObjectiveType {
-    GATHER_ITEMS,
+    COLLECT_RESOURCES,
     KILL_MOBS,
-    DELIVER_ITEM,
-    EXPLORE_LOCATION,
     FETCH_ITEM,
-    TALK_TO_NPC,
-    KILL_NPC
+    TALK_TO_NPC
 }
