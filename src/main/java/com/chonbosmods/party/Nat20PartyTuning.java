471ebb45a25c18ab4eda0f3dabb787e965a53026
chonbosmods <267707111+ChonbosMods@users.noreply.github.com>
2026-04-22 21:27:03 -0700

    feat(party): add party-tuning constants class

---
+++ src/main/java/com/chonbosmods/party/Nat20PartyTuning.java
@@ -0,0 +1,8 @@
+package com.chonbosmods.party;
+
+public final class Nat20PartyTuning {
+    private Nat20PartyTuning() {}
+
+    public static final double NAT20_PARTY_PROXIMITY = 80.0;
+    public static final int MLVL_PARTY_CAP = 6;
+}
